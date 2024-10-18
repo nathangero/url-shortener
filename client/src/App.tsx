dda1f31d515b108b102a32a961296bc859c61f91
Nathan Geronimo <nathangero@yahoo.com>
2024-10-18 12:15:54 -0700

    implement submitting form and show result (no backend yet)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,12 @@ import UrlForm from "./components/UrlForm"
 function App() {
 
   return (
-    <main className="flex-col items-center justify-center">
-      <h1 className="text-5xl">URL Shortener</h1>
+    <main className="flex justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl">URL Shortener</h1>
 
-      {UrlForm()}
+        {UrlForm()}
+      </div>
     </main>
   )
 }
--- client/src/components/UrlForm.tsx
+++ client/src/components/UrlForm.tsx
@@ -3,10 +3,11 @@ import { SyntheticEvent, useState } from "react"
 export default function UrlForm() {
 
   const [url, setUrl] = useState<string>('');
-
+  const [urlShortened, setUrlShortened] = useState<string>('');
 
   const onChangeUrl = (event: SyntheticEvent) => {
-    setUrl(event.currentTarget.textContent ?? "");
+    setUrl(event.target.value);
+    // setUrl(event.currentTarget.textContent ?? "");
   }
 
   const handleFormSubmit = (event: SyntheticEvent) => {
@@ -26,13 +27,20 @@ export default function UrlForm() {
 
           className="border-solid border-2 border-black w-64" required
           name="userName"
-          type="url"
+          // type="url"
           value={url}
           onChange={onChangeUrl}
         />
 
         <button type="submit" className="bg-blue-500 text-white cursor-pointer rounded text-xl p-2 mx-3">Shorten</button>
       </form>
+
+      {urlShortened &&
+        <>
+          <h3 className="text-2xl">Here's your shortened url</h3>
+          <p className="font-bold text-xl">{urlShortened}</p>
+        </>
+      }
     </div>
   )
 }

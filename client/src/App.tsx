d8ec7bc1659a521552f9971e8131612b7216c483
Nathan Geronimo <nathangero@yahoo.com>
2024-10-18 12:07:41 -0700

    create simple form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
+// import './index.css'
 import UrlForm from "./components/UrlForm"
 
 function App() {
 
   return (
-    <>
-      <h1>URL Shortener</h1>
+    <main className="flex-col items-center justify-center">
+      <h1 className="text-5xl">URL Shortener</h1>
 
       {UrlForm()}
-    </>
+    </main>
   )
 }
 
--- client/src/components/UrlForm.tsx
+++ client/src/components/UrlForm.tsx
@@ -1,13 +1,37 @@
+import { SyntheticEvent, useState } from "react"
 
 export default function UrlForm() {
 
+  const [url, setUrl] = useState<string>('');
 
+
+  const onChangeUrl = (event: SyntheticEvent) => {
+    setUrl(event.currentTarget.textContent ?? "");
+  }
+
+  const handleFormSubmit = (event: SyntheticEvent) => {
+    // Don't make the page refresh
+    event.preventDefault();
+    event.stopPropagation();
+
+
+  }
 
   return (
     <div>
-      <h2 className="font-bold">Enter your URL below, and click Shorten to shorten.</h2>
-      <form>
+      <h2 className="text-3xl">Enter your URL below, and click Shorten to shorten.</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          id="sUsername"
 
+          className="border-solid border-2 border-black w-64" required
+          name="userName"
+          type="url"
+          value={url}
+          onChange={onChangeUrl}
+        />
+
+        <button type="submit" className="bg-blue-500 text-white cursor-pointer rounded text-xl p-2 mx-3">Shorten</button>
       </form>
     </div>
   )

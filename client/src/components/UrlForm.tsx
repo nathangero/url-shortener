62cd2a361a90a841704442017c9f3487795cfb77
Nathan Geronimo <nathangero@yahoo.com>
2024-10-18 11:43:49 -0700

    setup tailwindcss

---
+++ client/src/components/UrlForm.tsx
@@ -0,0 +1,14 @@
+
+export default function UrlForm() {
+
+
+
+  return (
+    <div>
+      <h2 className="font-bold">Enter your URL below, and click Shorten to shorten.</h2>
+      <form>
+
+      </form>
+    </div>
+  )
+}
